a74b4cdd51817359392023180392596069f9819a
Apurv Gupta <apurv3351@gmail.com>
2021-06-03 20:29:45 +0530

    [new] shortcut keys

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy ✨🔥'
+git push -f git@github.com:apurv-1/sample-vue.git master:gh-pages
+cd -
